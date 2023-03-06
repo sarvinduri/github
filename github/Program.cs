d0e4b29648c24039db2ef28912ed529912eeac2b
admin <admin@44-1>
2023-03-06 13:44:25 +0300

    Добавьте файлы проекта.

---
+++ github/Program.cs
@@ -0,0 +1,14 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int[,] masd = new int[5,5];
+for (int i = 0;i < 5; i++)
+{
+    for (int j=0; j<5;j++)
+    {
+        if (i + j == 4)
+            masd[i, j] = i + 1;
+        Console.WriteLine(masd[i,j].ToString()+' ');
+    }
+    Console.WriteLine();
+}
